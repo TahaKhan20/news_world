6e05a318ffbee91f35386fd10cd32ced0f1b8cbc
Taha Khan <mohammadtahakhan20@gmail.com>
2023-08-03 12:40:59 +0500

    Initial Commits

--- news_world/src/App.js
+++ news_world/src/App.js
@@ -1,16 +1,8 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Nav1 from './components/navbar';
-import Home from './components/home'
 function App() {
   return (
     <div className="App">
-      
-        <Nav1 />
-        
-        <Routes>
-            <Route path = '/home' element = {<Home />}></Route>
-        </Routes>
+      <h1>News World</h1>
     </div>
   );
 }
